1331971495e1b8ac4445e501ce957aba22b13446
Carlos Vinicius <viniciusbr93@gmail.com>
2018-04-24 18:27:18 -0300

    Small adjustments on Liquibase

--- src/test/java/com/carlos/test/TestRepositoryTest.java
+++ src/test/java/com/carlos/test/TestRepositoryTest.java
@@ -1,17 +1,13 @@
 package com.carlos.test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-
 import javax.transaction.Transactional;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.carlos.entity.TestEntity;
 import com.carlos.repository.TestRepository;
 
